45fe9aebeb7ce8b4f8299f4a82267dada8fce5b9
Tyler Stanczak <tyler@Tylers-MacBook-Pro.local>
2019-10-11 09:00:31 -0500

    remove fs readfile in favor of export/require data

--- database/index.js
+++ database/index.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
 const {Schema} = require('mongoose');
-const {Model} = require('mongoose');
+const products = require('./data.js');
 
 const productSchema = new Schema({
-  image0: String,
+  prime_pic: String,
   image1: String,
   image2: String,
 });
-
-const product = Model('Product', productSchema);
-
-mongoose.connect('mongodb://localhost:27017/tbay');
+const Product = mongoose.model('Product', productSchema);
+mongoose.connect('mongodb://localhost:27017/tbay', {useNewUrlParser: true, useUnifiedTopology: true,});
+const db = mongoose.connection;
+// var products = JSON.parse(fs.readFileSync(__dirname + '/data.json', 'utf-8'));
+Product.insertMany(products);
