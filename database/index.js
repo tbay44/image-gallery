78e9462d470a22abae57a27b62910d959d4d2e0d
Tyler Stanczak <tyler@Tylers-MacBook-Pro.local>
2019-10-16 17:17:27 -0500

    Installed webpack, airbnb linter, and updated data with extra image URLs

--- database/index.js
+++ database/index.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
-const {Schema} = require('mongoose');
-const products = require('./data.js');
+// const {Schema} = require('mongoose');
+// const products = require('./data.js');
 
 const productSchema = new Schema({
   'prime_pic': String,
-  image1: String,
-  image2: String,
+  'pic_1': String,
+  'pic_2': String,
 });
 const Product = mongoose.model('Product', productSchema);
+
 const connectionString = 'mongodb+srv://tbay:tbay@tbay-pdiop.gcp.mongodb.net/tbay?retryWrites=true&w=majority';
 mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-Product.insertMany(products);
+
+// Product.insertMany(products);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import ReactDOM from 'react-dom';
+import App from './components/app.jsx'
 
 ReactDOM.render(<App/>, document.getElementById('image-gallery'));
